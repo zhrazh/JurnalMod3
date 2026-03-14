75edc5c01a645bbce4555d55e5287f74fa791869
zhrazh <zharaaaaa0@gmail.com>
2026-03-14 08:51:17 +0700

    Menambahkan implementasi konversi suhu

--- Modul3_103022400015/Form1.cs
+++ Modul3_103022400015/Form1.cs
@@ -12,6 +12,8 @@ namespace Modul3_103022400015
 {
     public partial class Form1 : Form
     {
+        double hasil = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -64,7 +66,56 @@ namespace Modul3_103022400015
 
         private void buttonConvert_Click(object sender, EventArgs e)
         {
+            if (comboBoxSatuanAwal.Text == "" || comboBoxSatuanAkhir.Text == "")
+            {
+                MessageBox.Show("Pilih satuan terlebih dahulu!");
+                return;
+            }
 
+            double nilaiAwal;
+            if (!double.TryParse(textBoxNilaiAwal.Text, out nilaiAwal))
+            {
+                MessageBox.Show("Masukkan angka yang valid!");
+                return;
+            }
+
+            string satuanAwal = comboBoxSatuanAwal.Text;
+            string satuanAkhir = comboBoxSatuanAkhir.Text;
+            double hasil = 0;
+
+            if (satuanAwal == "Celcius" && satuanAkhir == "Celcius")
+                hasil = nilaiAwal;
+            else if (satuanAwal == "Celcius" && satuanAkhir == "Fahrenheit")
+                hasil = (nilaiAwal * 9 / 5) + 32;
+            else if (satuanAwal == "Celcius" && satuanAkhir == "Kelvin")
+                hasil = nilaiAwal + 273.15;
+            else if (satuanAwal == "Celcius" && satuanAkhir == "Reamur")
+                hasil = nilaiAwal * 4 / 5;
+            else if (satuanAwal == "Fahrenheit" && satuanAkhir == "Celcius")
+                hasil = (nilaiAwal - 32) * 5 / 9;
+            else if (satuanAwal == "Fahrenheit" && satuanAkhir == "Fahrenheit")
+                hasil = nilaiAwal;
+            else if (satuanAwal == "Fahrenheit" && satuanAkhir == "Kelvin")
+                hasil = (nilaiAwal - 32) * 5 / 9 + 273.15;
+            else if (satuanAwal == "Fahrenheit" && satuanAkhir == "Reamur")
+                hasil = (nilaiAwal - 32) * 4 / 9;
+            else if (satuanAwal == "Kelvin" && satuanAkhir == "Celcius")
+                hasil = nilaiAwal - 273.15;
+            else if (satuanAwal == "Kelvin" && satuanAkhir == "Fahrenheit")
+                hasil = (nilaiAwal - 273.15) * 9 / 5 + 32;
+            else if (satuanAwal == "Kelvin" && satuanAkhir == "Kelvin")
+                hasil = nilaiAwal;
+            else if (satuanAwal == "Kelvin" && satuanAkhir == "Reamur")
+                hasil = (nilaiAwal - 273.15) * 4 / 5;
+            else if (satuanAwal == "Reamur" && satuanAkhir == "Celcius")
+                hasil = nilaiAwal * 5 / 4;
+            else if (satuanAwal == "Reamur" && satuanAkhir == "Fahrenheit")
+                hasil = (nilaiAwal * 9 / 4) + 32;
+            else if (satuanAwal == "Reamur" && satuanAkhir == "Kelvin")
+                hasil = (nilaiAwal * 5 / 4) + 273.15;
+            else if (satuanAwal == "Reamur" && satuanAkhir == "Reamur")
+                hasil = nilaiAwal;
+            textBoxNilaiAkhir.Text = hasil.ToString();
         }
     }
 }
